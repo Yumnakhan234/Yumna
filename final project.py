7483961bd0d176ed9aa97111cfa423826f02290c
Yumna aman <amanlala997@gmail.com>
2024-12-15 21:02:39 +0500

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,20 @@
+import random
+
+my_quotes = ["Forgiveness is choosing to be happy.",
+             
+"Forgiveness is simply freeing ourselves from wanting to punish.",
+
+"We experience what we intend for others.",
+
+"Fear is wisdom as a child.",
+
+"patience is a key element of success.",
+
+"your most unhappy costumers are your greatest source of learning.",]
+
+random_quotes = random.choice(my_quotes)
+print()
+print("your random quote is: ")
+print()
+print(random_quotes)
+print()
